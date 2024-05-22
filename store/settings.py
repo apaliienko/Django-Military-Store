c5f12521c798a4818e1e9b04ce8c748661a5f1fb
Artem <a.ot.worker@gmail.com>
2024-05-22 14:10:48 +0300

    Connect Celery

--- store/settings.py
+++ store/settings.py
@@ -173,7 +173,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-en'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
